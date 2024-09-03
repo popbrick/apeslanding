92a6390120d1a3bdda5c1192bd82d1a9cd6db98c
jedinrk <jedi.nrk@gmail.com>
2024-09-04 00:49:39 +0530

    removed template bg and added gradient per design

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { Navbar } from "./Navbar";
 import { Banner } from "./Banner";
+import BackgroundImage from "../public/background_image.png"
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-gradient-to-t from-[#181a1e] to-[#181a1e]">
+      {/* <Image src={BackgroundImage} alt="BackgroundImage" className="absolute z-0 w-full"/> */}
       <Navbar/>
+      <Banner />
+      <Banner />
       <Banner />
     </main>
   );
