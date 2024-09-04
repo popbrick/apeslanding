2b6fba2adf141ea21d2e5c3748d402489000eca5
jedinrk <jedi.nrk@gmail.com>
2024-09-05 00:14:29 +0530

    Adding Background Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
 import { Navbar } from "./Navbar";
 import { Banner } from "./Banner";
-import BackgroundImage from "../public/background_image.png"
+import BackgroundImage from "../public/background_image.png";
 import { Announcements } from "./Announcements";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-t from-[#181a1e] to-[#181a1e]">
-      {/* <Image src={BackgroundImage} alt="BackgroundImage" className="absolute z-0 w-full"/> */}
-      <Navbar/>
-      <Banner />
-      <Announcements />
+    <main className="relative min-h-screen bg-gradient-to-t from-[#181a1e] to-[#181a1e] overflow-hidden">
+      <Image
+        src={BackgroundImage}
+        alt="BackgroundImage"
+        className="absolute inset-0 w-full object-cover z-0"
+      />
+      <div className="relative z-10">
+        <Navbar />
+        <Banner />
+        <Announcements />
+      </div>
     </main>
   );
 }
