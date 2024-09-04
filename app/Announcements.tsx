6ba59dbd50c85f5dbf07b1da5484c7e43c3ae79c
jedinrk <jedi.nrk@gmail.com>
2024-09-04 15:52:59 +0530

    Fixed Card styles

--- app/Announcements.tsx
+++ app/Announcements.tsx
@@ -10,7 +10,7 @@ export const Announcements: React.FC = () => {
       <div className="w-full max-w-[1320px] m-auto flex flex-col gap-20 px-10">
         <h2 className={`${poppins.className} text-center text-white text-5xl font-medium leading-[62.40px]`}>While waiting for the Big Day</h2>
         <div className="grid gap-[52px] md:grid-cols-3">
-          <AnnouncementCard step={1} backgroundImage={bgCardEarlyAccess.src} label={'Submit forEarly Access now!'} revealed={true}/>
+          <AnnouncementCard step={1} backgroundImage={bgCardEarlyAccess.src} label="Submit for <br/> Early Access now!" revealed={true}/>
           <AnnouncementCard step={2} backgroundImage={''} label={'To be revealed'} revealed={false}/>
           <AnnouncementCard step={3} backgroundImage={''} label={'To be revealed'} revealed={false}/>
         </div>
--- app/components/AnnouncementCard.tsx
+++ app/components/AnnouncementCard.tsx
@@ -25,32 +25,19 @@ export const AnnouncementCard: React.FC<AnnouncementProps> = ({
         })`,
       }}
     >
-      <div
-        className={`absolute top-4 left-4 w-8 h-8 flex items-center justify-center rounded-full bg-white text-[28px] text-black font-bold ${poppins.className}`}
-      >
-        {step}
+      <div className="w-14 h-14 relative bg-white/20 rounded-full border-4 border-white/50 backdrop-blur-[50px] flex items-center justify-center">
+        <div
+          className={`text-center text-white text-[28px] font-bold ${poppins.className}`}
+        >
+          {step}
+        </div>
       </div>
-      <div
-        className={`absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white text-[26px] font-semibold ${inter.className}`}
-      >
-        {label}
+      <div className="absolute bottom-4 left-1/2 w-[calc(100%-32px)] transform -translate-x-1/2 px-4 pb-9">
+        <div
+          className={`text-white text-[26px] font-semibold text-center ${inter.className} leading-9`}
+          dangerouslySetInnerHTML={{ __html: label }}
+        />
       </div>
     </div>
   );
 };
-
-{
-  /* <div className={`p-4 rounded-3xl relative`}>
-      <Image src={BgDefault} alt="default" className="absolute z-0"/>
-      <div className="w-14 h-14 relative bg-white/20 rounded-[500px] border-4 border-white/50 backdrop-blur-[50px]">
-        <div className={`${poppins.className} left-[23px] top-[7px] absolute text-center text-white text-[28px] font-bold`}>
-          {step}
-        </div>
-      </div>
-      <div
-        className={`${inter.className} text-center text-white text-[26px] font-semibold leading-9`}
-      >
-        {label}
-      </div>
-    </div> */
-}
