4daabd63718ad29f9fb106c7a4a4fcea837288dd
jedinrk <jedi.nrk@gmail.com>
2024-09-05 11:34:26 +0530

    removed white background before and after page scroll

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import { Announcements } from "./Announcements";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen bg-gradient-to-t from-[#181a1e] to-[#181a1e]">
+    <main className="relative min-h-screen bg-gradient-to-t from-[#181a1e] to-[#181a1e] pb-52">
       <Image
         src={BackgroundImage}
         alt="BackgroundImage"
-        className="absolute inset-0 w-full object-cover z-0"
+        className="absolute inset-0 w-full h-full object-cover z-0"
       />
       <div className="relative z-10">
         <Navbar />
