73f0e3c0e2d9bb752bde912611bf7aeb37d51ca0
jedinrk <jedi.nrk@gmail.com>
2024-09-24 17:16:23 +0530

    Fixed Count down timer timezone

--- app/Banner.tsx
+++ app/Banner.tsx
@@ -6,7 +6,7 @@ import { GetEarlyAccessModal } from "./components/GetEarlyAccessModal";
 import { inter, poppins } from "./fonts";
 
 export const Banner = () => {
-  const targetDate = "2024-09-24T19:00:00"; //24th September 19.00 UTC
+  const targetDate = "2024-09-24T19:00:00Z"; //24th September 19.00 UTC
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => {
